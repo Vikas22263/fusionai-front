5075c01ed4d1cfd81c35457e0495252c3e70bad5
vikas patel <vikash22263@gmail.com>
2025-03-10 01:47:59 +0530

    added g

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -4,7 +4,7 @@ let socket = null;
 export const getWebSocket = () => {
   if (!socket || socket.readyState === WebSocket.CLOSED || socket.readyState === WebSocket.CLOSING) {
     try {
-      socket = new WebSocket('http://193.203.161.214:9001');
+      socket = new WebSocket('wss://193.203.161.214:9001');
     } catch (error) {
       console.error('WebSocket connection failed:', error);
     }
