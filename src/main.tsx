dc9ce837140a312982758f1166c07b8d0fba844d
vikas patel <vikash22263@gmail.com>
2025-03-09 19:09:38 +0530

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store.ts";
-import { WebSocketProvider } from "./context/socket.tsx";
+
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
